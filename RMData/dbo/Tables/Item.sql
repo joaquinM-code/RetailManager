89fa80bf52a923e3962a312868f13a672320c485
ecatom <netpassrecovers@hotmail.com>
2021-08-26 09:02:55 +0200

    Creating SQL Tables
    Creation of the initial DB structure

---
+++ RMData/dbo/Tables/Item.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Item]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ItemName] NVARCHAR(100) NOT NULL, 
+    [Description] NVARCHAR(MAX) NULL,
+    [RetailPrice] MONEY NOT NULL,   
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
---
+++ RMData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [SaleId] INT NOT NULL, 
+    [Quantity] INT NOT NULL,
+    [ItemId] INT NOT NULL, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [Tax] MONEY NOT NULL DEFAULT 0, 
+)
