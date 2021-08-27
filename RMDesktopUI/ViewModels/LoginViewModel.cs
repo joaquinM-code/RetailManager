92c44a84f8dd2c0c711a62622e289f898f8290b2
ecatom <netpassrecovers@hotmail.com>
2021-08-27 12:40:35 +0200

    API Wiring + Form Ligin Management

---
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Caliburn.Micro;
+using RMDesktopUI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _userName;
+        private string _password;
+        private IAPIHelper _apiHelper;
+
+        public LoginViewModel(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+        public string UserName
+        {
+            get { return _userName; }
+            set 
+            { 
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set 
+            { 
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLoginButton);
+            }
+        }
+
+        public bool CanLoginButton
+        {
+            get
+            {
+                bool output = false;
+                if (!string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(Password))
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+        }
+
+        public async Task LoginButton ()
+        {
+            try
+            {
+                var result = await _apiHelper.Authenticate(UserName, Password);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+
+
+    }
+}
--- RMDesktopUI/ViewModels/ShellViewModel.cs
+++ RMDesktopUI/ViewModels/ShellViewModel.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace RMDesktopUI.ViewModels
 {
-    class ShellViewModel
+    class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loginVM;
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            _loginVM = loginVM;
+            ActivateItemAsync(_loginVM);
+        }
     }
 }
