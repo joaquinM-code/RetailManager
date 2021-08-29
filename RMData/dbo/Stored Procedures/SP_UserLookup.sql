9f2e7809811fc740e5b8b6ca718eaf551dd3d9e9
ecatom <netpassrecovers@hotmail.com>
2021-08-29 09:55:31 +0200

    Wired User To API
    Created class Library for sql connections and wired UserModel to API

---
+++ RMData/dbo/Stored Procedures/SP_UserLookup.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[SP_UserLookup]
+	@Id nvarchar(128)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT FirstName, LastName, EmailAddress, CreatedDate 
+	FROM [dbo].[User] 
+	WHERE AuthUserId = @Id
+
+END;
---
+++ RMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using RMDataManager.Library.Internal.DataAccess;
+using RMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var parameters = new { Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.SP_UserLookup", parameters, "RMData");
+
+            return output;
+        }
+    }
+}
